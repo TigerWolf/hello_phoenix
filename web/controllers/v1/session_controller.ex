485bd0357af2717dd5e95a66ef25b1b0d2be7662
Kieran <TigerWolf@users.noreply.github.com>
2015-10-22 20:59:34 +1030

    Display user when login

--- web/controllers/v1/session_controller.ex
+++ web/controllers/v1/session_controller.ex
@@ -2,11 +2,13 @@ defmodule HelloPhoenix.V1.SessionController do
   use HelloPhoenix.Web, :controller
 
   alias HelloPhoenix.Session
+  alias HelloPhoenix.User
 
   plug HelloPhoenix.Plugs.Authenticated
 
   def create(conn, _params) do
-    render(conn, "index.json")
+    user = Repo.get!(User, conn.assigns.user_id) |> Repo.preload([:team])
+    render(conn, "index.json", user: user)
   end
-  
+
 end
--- web/views/v1/session_view.ex
+++ web/views/v1/session_view.ex
@@ -1,8 +1,18 @@
 defmodule HelloPhoenix.V1.SessionView do
   use HelloPhoenix.Web, :view
 
-  def render("index.json", params) do
-    %{data: "true"}
+  def render("index.json", %{user: user}) do
+    %{data:
+      %{
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        team: team_name(user.team)
+      }
+    }
   end
+
+  def team_name(nil), do: ""
+  def team_name(team), do: team.name
 
 end
