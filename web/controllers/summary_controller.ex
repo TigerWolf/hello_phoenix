5c7656a8e3700f86abe939894dd1d3238847602d
Kieran <TigerWolf@users.noreply.github.com>
2015-10-28 15:33:12 +1030

    Added team name to csv

--- web/controllers/summary_controller.ex
+++ web/controllers/summary_controller.ex
@@ -10,7 +10,7 @@ defmodule HelloPhoenix.SummaryController do
 
   def csv(conn, _params) do
     logs = Repo.all(Log)
-      |> Repo.preload([:activity, :user])
+      |> Repo.preload([:activity, user: :team])
       |> Enum.map( fn(x) ->
         [
           x.id,
@@ -22,13 +22,19 @@ defmodule HelloPhoenix.SummaryController do
           x.user.name,
           x.user.email,
           x.inserted_at |> Calendar.DateTime.shift_zone!("Australia/Adelaide") |> Calendar.Strftime.strftime!("%A, %e %B %Y"),
-          x.amount * x.activity.points
+          x.amount * x.activity.points,
+          x.user.team_id,
+          team_name(x.user.team)
         ]
       end)
       |> CSVLixir.write
       |> Enum.to_list
-      |> List.insert_at(0, ["id,amount,activity_id,activity name,activity points,user id, name, email, date, points sub total\r\n"])
+      |> List.insert_at(0, ["id,amount,activity_id,activity name,activity points,user id, name, email, date, points sub total, team id,\r\n"])
     conn |> put_resp_content_type("text/csv") |> send_resp(200, logs)
   end
+
+  def team_name(nil), do: ""
+  def team_name(team), do: team.name
+
 
 end
